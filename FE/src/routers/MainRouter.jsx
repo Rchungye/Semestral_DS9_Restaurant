b416889cc1f84513648d8d3b201cd607182474cb
UzzielAp <uzzielapariciok01@gmail.com>
2025-07-24 22:09:36 -0500

    No Necesitamos esa pantalla de iniciar sesion

--- FE/src/routers/MainRouter.jsx
+++ FE/src/routers/MainRouter.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import PedirYa from "../pages/PedirYa";
-import IniciarSesion from "../pages/IniciarSesion";
 import Kitchen from "../pages/kitchen"
 import LoginPage from "../pages/Login";
 import Admin from "../pages/Admin";
@@ -11,7 +10,6 @@ const MainRouter = () => {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/pedir-ya" element={<PedirYa />} />
-            <Route path="/iniciar-sesion" element={<IniciarSesion />} />
             <Route path="/kitchen" element={<Kitchen />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/admin" element={<Admin />} />
