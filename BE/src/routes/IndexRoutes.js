0047f1b46d053015961bb2aa9c27d205ff881b16
Rafael Chung <rafaelchung28@hotmail.com>
2025-07-27 03:08:15 -0500

    fix routes

--- BE/src/routes/IndexRoutes.js
+++ BE/src/routes/IndexRoutes.js
@@ -1,12 +1,12 @@
 // src/routes/IndexRoutes.js
-import ProductRoutes from './ProductRoutes.js'
 import DishRoutes from './DishRoutes.js'
+import PromotionRoutes from './PromotionRoutes.js'
 import TableRoutes from './TableRoutes.js'
 import UserRoutes from './UserRoutes.js'
 
 export default function (fastify) {
-    ProductRoutes(fastify)
     DishRoutes(fastify)
     TableRoutes(fastify)
     UserRoutes(fastify)
+    PromotionRoutes(fastify)
 }
